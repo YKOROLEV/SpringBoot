f37c60c626ff42a829fed3687dc80567721b5040
Ярослав Королев <1201576@gmail.com>
2020-02-08 23:18:12 +0300

    RestService check exception

--- src/main/java/ru/korolev/springboot/service/RestServiceImpl.java
+++ src/main/java/ru/korolev/springboot/service/RestServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import ru.korolev.springboot.model.dto.UserDTO;
 
@@ -15,93 +16,114 @@ import java.util.Optional;
 @Service
 public class RestServiceImpl implements RestService {
 
-    private RestTemplate restTemplate;
-
     private final RestTemplateBuilder restTemplateBuilder;
+    private RestTemplate restTemplate;
 
     public RestServiceImpl(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplateBuilder = restTemplateBuilder;
-    }
-
-    public void init(String login, String password) {
-        this.restTemplate = restTemplateBuilder
-                .basicAuthentication(login, password)
-                .build();
     }
 
     @Override
     public Optional<UserDTO> authorization(String login, String password) {
         init(login, password);
 
-        ResponseEntity<UserDTO> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8081/api/v1/authorization?login={login}&password={password}",
-                UserDTO.class,
-                login,
-                password
-        );
+        try {
+            ResponseEntity<UserDTO> response = restTemplate.getForEntity(
+                    "http://localhost:8081/api/v1/authorization?login={login}&password={password}",
+                    UserDTO.class,
+                    login,
+                    password
+            );
 
-        return responseEntity.getStatusCode() == HttpStatus.OK
-                ? Optional.ofNullable(responseEntity.getBody()) : Optional.empty();
+            return response.getStatusCode() == HttpStatus.OK
+                    ? Optional.ofNullable(response.getBody()) : Optional.empty();
+        } catch (HttpStatusCodeException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<UserDTO> getAll() {
-        ResponseEntity<UserDTO[]> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8081/api/v1/user/all",
-                UserDTO[].class
-        );
+        try {
+            ResponseEntity<UserDTO[]> response = restTemplate.getForEntity(
+                    "http://localhost:8081/api/v1/user/all",
+                    UserDTO[].class
+            );
 
-        if (responseEntity.getBody() == null) {
+            if (response.getBody() == null) {
+                return Collections.emptyList();
+            }
+
+            return response.getStatusCode() == HttpStatus.OK
+                    ? Arrays.asList(response.getBody()) : Collections.emptyList();
+        } catch (HttpStatusCodeException e) {
             return Collections.emptyList();
         }
-
-        return responseEntity.getStatusCode() == HttpStatus.OK
-                ? Arrays.asList(responseEntity.getBody()) : Collections.emptyList();
     }
 
     @Override
     public Optional<UserDTO> getById(Long id) {
-        ResponseEntity<UserDTO> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8081/api/v1/user/{id}",
-                UserDTO.class,
-                id
-        );
-
-        return responseEntity.getStatusCode() == HttpStatus.OK
-                ? Optional.ofNullable(responseEntity.getBody()) : Optional.empty();
+        try {
+            ResponseEntity<UserDTO> response = restTemplate.getForEntity(
+                    "http://localhost:8081/api/v1/user/{id}",
+                    UserDTO.class,
+                    id
+            );
+            return response.getStatusCode() == HttpStatus.OK
+                    ? Optional.ofNullable(response.getBody()) : Optional.empty();
+        } catch (HttpStatusCodeException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<UserDTO> create(UserDTO userDTO) {
-        ResponseEntity<UserDTO> responseEntity = restTemplate.postForEntity(
-                "http://localhost:8081/api/v1/user/create",
-                userDTO,
-                UserDTO.class
-        );
-
-        return responseEntity.getStatusCode() == HttpStatus.OK
-                ? Optional.ofNullable(responseEntity.getBody()) : Optional.empty();
+        try {
+            ResponseEntity<UserDTO> response = restTemplate.postForEntity(
+                    "http://localhost:8081/api/v1/user/create",
+                    userDTO,
+                    UserDTO.class
+            );
+            return response.getStatusCode() == HttpStatus.OK
+                    ? Optional.ofNullable(response.getBody()) : Optional.empty();
+        } catch (HttpStatusCodeException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public HttpStatus update(UserDTO userDTO) {
-        ResponseEntity<?> responseEntity = restTemplate.postForEntity(
-                "http://localhost:8081/api/v1/user/update",
-                userDTO,
-                UserDTO.class
-        );
+        try {
+            ResponseEntity<?> response = restTemplate.postForEntity(
+                    "http://localhost:8081/api/v1/user/update",
+                    userDTO,
+                    UserDTO.class
+            );
 
-        return responseEntity.getStatusCode();
+            return response.getStatusCode();
+        } catch (HttpStatusCodeException e) {
+            return e.getStatusCode();
+        }
     }
 
     @Override
     public HttpStatus delete(Long id) {
-        ResponseEntity<?> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8081/api/v1/user/delete/{id}",
-                ResponseEntity.class,
-                id
-        );
+        try {
+            ResponseEntity<?> response = restTemplate.getForEntity(
+                    "http://localhost:8081/api/v1/user/delete/{id}",
+                    ResponseEntity.class,
+                    id
+            );
 
-        return responseEntity.getStatusCode();
+            return response.getStatusCode();
+        } catch (HttpStatusCodeException e) {
+            return e.getStatusCode();
+        }
+    }
+
+    private void init(String login, String password) {
+        this.restTemplate = restTemplateBuilder
+                .basicAuthentication(login, password)
+                .build();
     }
 }
